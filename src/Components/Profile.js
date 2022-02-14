aa934c985736d719b3763f82aaabde16e5601e0e
unknown <cantilloderek@gmail.com>
2022-02-13 19:49:06 -0500

    otro commit mas xd

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -11,60 +11,7 @@ const Profile=()=> {
       
       ];
   return (
-    {/* <div className='profile-container'>
-        <div className='profile-parent'>
-            <div className='profile-details'>
-                <div className='colz'>
-                    <div className='colz-icon'>
-                        <a href='https://www.linkedin.com/in/derek-cantillo-34bb81213/' target="_blank">
-                            <i className='fa fa-linkedin-square'></i>
-                        </a>
-                        <a href='https://github.com/derekcantillo' target="_blank">
-                            <i className='fa fa-github-square'></i>
-                        </a>
-                        <a href='https://www.instagram.com/dereskeree/' target="_blank">
-                            <i className='fa fa-instagram'></i>
-                        </a>
-                     
-                    </div>
-                    
-                </div>
-                <div className='profile-details-name'>
-                    <span className='primary-text'>
-                        {" "}
-                        Hola Mundo, soy <span className='highlighted-text'>Derek</span>
-                    </span>
- 
-                </div>
-                <div className='profile-details-role'>
-                    <span className='primary-text'>
-                        {" "} <h1> {" "}
-                        <Typical loop={Infinity} 
-                        steps={steps}></Typical></h1>
-                        <span className='profile-role-tagline'>Abierto al mundo del desarrollo!</span>
-                    </span>
-
-                </div>
-                <div className='profile-options'>
-
-                    <button className='btn primary-btn'>{" "}Contáctame{" "}</button>
-                    <a href="cvderekweb.pdf" download="Hoja de Vida Derek.pdf">
-                    <i className="bi bi-download"></i>
-                        <button className='btn highlighted-btn'>
-                        Currículum</button></a>
-                    
-                </div>
-
-                
-            </div>
-            <div className='profile-picture'>
-                    <div className='profile-picture-background'></div>
-                    
-
-                </div>
-        </div>
-        
-    </div> */},
+    
 
     <div id='profile'>
         <Header/>
@@ -90,10 +37,10 @@ const Profile=()=> {
                 <div className='header-btns '>
 
                     <button className='cv-btn1'>{" "}Contáctame{" "}</button>
-                    <a href="cvderekweb.pdf"  download="Hoja de Vida Derek.pdf">
+                    <a href="hvderekcantilloweb.pdf"  download="Hoja de Vida Derek.pdf">
                    
                         <button className='cv-btn'>
-                            
+
                         Currículum</button></a>
                     
                 </div>
